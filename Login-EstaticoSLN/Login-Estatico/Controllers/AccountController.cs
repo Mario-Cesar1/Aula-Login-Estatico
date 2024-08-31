0cc02d96b3a308fd688c4b5d78c5d327efc92649
Mario-Cesar1 <115711742+Mario-Cesar1@users.noreply.github.com>
2024-08-31 11:45:02 -0300

    a

---
+++ Login-EstaticoSLN/Login-Estatico/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Login_Estatico.Controllers
+{
+    public class AccountController : Controller
+    {
+        private const string StaticUsername = "admin";
+        private const string StaticPassword = "1234";
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string username, string Password)
+        {
+            if (username == StaticUsername && Password == StaticPassword)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewBag.Error = "Usuário ou senha inválidos!";
+            return View();
+        }
+    }
+}
